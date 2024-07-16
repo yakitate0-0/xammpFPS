69890436f196d03c1725d4a0db180d0a9326fb84
yakitate0-0 <146817806+yakitate0-0@users.noreply.github.com>
2024-07-16 21:51:22 +0900

    php

---
+++ php/create.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>create Account</title>
+</head>
+
+<body>
+    <p>登録するユーザー情報を入力してください。</p>
+    <form action="create.php" method="POST">
+        ID:<input type="text" name="id"><br>
+        パスワード:<input type="password" name="pass"><br>
+        名前:<input type="text" name="name"><br>
+        <input type="submit" value="送信">
+    </form>
+    <br>
+
+
+    <?php
+
+    
+
+    ?>
+
+</body>
+
+</html>
