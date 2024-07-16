6cab6dc1b6bfba7bf36d11d79dfb20e3d5f80df9
yakitate0-0 <146817806+yakitate0-0@users.noreply.github.com>
2024-07-16 22:25:25 +0900

    見た目の修正

--- php/create.php
+++ php/create.php
@@ -5,59 +5,66 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>create Account</title>
+    <link rel="stylesheet" href="./create.css">
+    <link href="https://fonts.googleapis.com/css2?family=Audiowide&family=Orbitron:wght@400;700;900&display=swap"
+        rel="stylesheet">
 </head>
 
 <body>
-    <p>登録するユーザー情報を入力してください。</p>
-    <form action="create.php" method="POST">
-        ID:<input type="text" name="id"><br>
-        パスワード:<input type="password" name="pass"><br>
-        名前:<input type="text" name="name"><br>
-        <input type="submit" value="送信">
-    </form>
-    <br>
+    <div class="glitch-overlay"></div>
+    <div class="scanner"></div>
+    <div class="container">
+
+        <h3>Enter your information to register</h3>
+        <form action="create.php" method="POST">
+            ID:<input type="text" name="id"><br>
+            Password:<input type="password" name="pass"><br>
+            Name:<input type="text" name="name"><br>
+            <input type="submit" value="create">
+        </form>
+        <br>
 
 
-    <?php
+        <?php
 
-    if (!empty($_POST["id"]) && !empty($_POST["pass"]) && !empty($_POST["name"])) {
-        try {
-            $pdo = new PDO(
-                "mysql:host=localhost;dbname=FPSplayer;",
-                "root",
-                "",
-                [
-                    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-                    PDO::ATTR_EMULATE_PREPARES => false
-                ]
-            );
+        if (!empty($_POST["id"]) && !empty($_POST["pass"]) && !empty($_POST["name"])) {
+            try {
+                $pdo = new PDO(
+                    "mysql:host=localhost;dbname=FPSplayer;",
+                    "root",
+                    "",
+                    [
+                        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                        PDO::ATTR_EMULATE_PREPARES => false
+                    ]
+                );
 
-            $sql = "INSERT INTO login (id, pass, name)VALUES(:id, :pass, :name);";
-            $stmt = $pdo->prepare($sql);
-            $stmt->bindValue('id', $_POST["id"], PDO::PARAM_STR);
+                $sql = "INSERT INTO login (id, pass, name)VALUES(:id, :pass, :name);";
+                $stmt = $pdo->prepare($sql);
+                $stmt->bindValue('id', $_POST["id"], PDO::PARAM_STR);
 
-            $pass = password_hash($_POST["pass"], PASSWORD_DEFAULT);
-            $stmt->bindValue('pass', $pass, PDO::PARAM_STR);
-            $stmt->bindValue('name', $_POST["name"], PDO::PARAM_STR);
+                $pass = password_hash($_POST["pass"], PASSWORD_DEFAULT);
+                $stmt->bindValue('pass', $pass, PDO::PARAM_STR);
+                $stmt->bindValue('name', $_POST["name"], PDO::PARAM_STR);
 
 
-            $result = $stmt->execute();
+                $result = $stmt->execute();
 
-            if ($result == true) {
-                print ("<br><br>登録しました<br>");
-                print ("ID: " . $_POST["id"] . "<br>");
-                print ("ID: " . $_POST["name"] . "<br>");
-            } else {
-                print ("<br><br>登録に失敗しました<br>");
+                if ($result == true) {
+                    print ("<br><br>Successful creation!!<br>");
+                    print ("ID: " . $_POST["id"] . "<br>");
+                    print ("ID: " . $_POST["name"] . "<br>");
+                } else {
+                    print ("<br><br>Creation failed!!<br>");
+                }
+
+            } catch (Exception $e) {
+                print ($e->getMessage() . "<br>");
             }
-
-        } catch (Exception $e) {
-            print ($e->getMessage() . "<br>");
         }
-    }
 
-    ?>
+        ?>
 
 </body>
 
