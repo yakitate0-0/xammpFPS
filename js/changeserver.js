a3101def4df4bc24bd5df2115b86a2cde2ccd822
yakitate0-0 <146817806+yakitate0-0@users.noreply.github.com>
2024-07-17 21:15:27 +0900

    playerとresultを受け取る

--- js/changeserver.js
+++ js/changeserver.js
@@ -4,16 +4,19 @@ document.getElementById('changePortButton').addEventListener('click', function (
     // 新しいポート番号を指定
     const newPort = 3000;
     // 新しいURLにリダイレクト
-    // window.location.href = `http://${host}:${newPort}`;
+    window.location.href = `http://${host}:${newPort}?id=${window.loggedInUser.id}`;
 });
 
 const params = new URLSearchParams(window.location.search);
 const result = params.get('result');
+const playerid = params.get('playerid');
 const url = new URL(window.location);
 url.searchParams.delete('result');
+url.searchParams.delete('playerid');
 window.history.replaceState({}, document.title, url);
 
 // 取得した値を表示
 if (result) {
     console.log(result);
+    console.log(playerid);
 }
